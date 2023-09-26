0d70ae90143e2193dcf456c3dff72f5b5c4448a5
Gustavo Gorges Koch <gustavogk@weg.net>
2023-09-26 16:54:00 -0300

    parte beneficios

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
+  content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#072139",
+        back: "#F3F3F3",
+        orange: "#E38E29",
+        lightBlue: "#4e8ac2",
+      },
+    },
   },
   plugins: [],
-}
+};
