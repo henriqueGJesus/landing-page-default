299adf889e943724d5c7c282b6d672ffae2b6a6a
Matheus Becker <beckerm@weg.net>
2023-09-26 16:56:19 -0300

    footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,14 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#072139',
+        'back': '#F3F3F3',
+        'orange': '#E38E29',
+        'lightBlue': '#4e8ac2'
+      },
+    },
   },
   plugins: [],
 }
