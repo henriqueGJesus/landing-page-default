d16b3c786e613f18e215ff6989bbede9ed7d7973
Henrique Goncalves de Jesus <henriquegj@weg.net>
2023-09-26 16:59:04 -0300

    henrique

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: [
+      "./src/**/*.{html,ts}",
+    ],
+    theme: {
+      extend: {
+        colors: {
+          'primary': '#072139',
+          'back': '#F3F3F3',
+          'orange': '#E38E29',
+          'lightBlue': '#4e8ac2'
+        },
+      },
+    },
+    plugins: [],
+  }
